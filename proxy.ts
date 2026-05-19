849d9edd7c02331d39a47133afc8b213e73fd2e8
Piyush Raj <sciencetechoindia@gmail.com>
2026-05-19 12:26:50 +0530

    Add initial pages for Home, SocialShare, and VideoUpload; implement Clerk authentication for sign-in and sign-up

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,39 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/sign-up(.*)',
+  "/",
+  "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+  "/api/videos"
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+  const {userId} = await auth()
+  const currentUrl = new URL(req.url)
+  const isAccessingDashboard = currentUrl.pathname === "/home"
+  const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url))
   }
+
+  // not logged in
+  if (!userId) {
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+    
+    if (!isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+  }
+
+  return NextResponse.next()
 })
 
 export const config = {
